e5864a2c850a31ab9b8864c30d835b3505315131
damu <wjl@126.com>
2020-03-12 09:29:25 +0800

    0312 -1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-contact-card
+            @click="clickFn"
+            type="add"
+            name="damu"
+            tel="15851802777"
+            add-text="add"></van-contact-card>
+
+    <van-contact-list
+            @add="addFn"
+            @edit="editFn"
+            @select="selectFn"
+            default-tag-text="尚硅谷彭于晏"
+            v-model="id"
+            :list="list"></van-contact-list>
+
+    <!--<van-contact-edit></van-contact-edit>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import { ContactCard, ContactList, ContactEdit } from 'vant';
+  export default {
+    name: 'App',
+    data(){
+       return {
+           id:3,
+           list:[
+               {id:1,name:"damu",tel:"15851802777",isDefault:true},
+               {id:2,name:"xfz",tel:"15851802778"},
+               {id:3,name:"qhf",tel:"15851802779"}
+           ]
+       }
+    } ,
+    components:{
+        [ContactCard.name]:ContactCard,
+        [ContactList.name]:ContactList,
+        [ContactEdit.name]:ContactEdit
+    },
+    methods:{
+        clickFn(){
+            console.log("---")
+        },
+        addFn(){console.log("addFn")},
+        editFn(item,index){console.log("editFn",item,index)},
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+        // selectFn:点击后面的小圆点选中的时候 当前函数会被处罚两次
+        selectFn(item,index){console.log("selectFn",item,index)}
+    }
   }
-}
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
